c689ce42212e4b109883e084adbd977f47eaeb63
Charley <ctpugmire@gmail.com>
2023-02-03 15:35:37 -0600

    add timestamp of last build

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addGlobalData('builtAt', () => {
+    let now = new Date();
+    return new Intl.DateTimeFormat('en-US', {
+      dateStyle: 'medium',
+      timeStyle: 'long',
+    }).format(now);
+  });
+
   return {
     dir: {
       data: '../_data',
