143bc582b13ec28bd5339d20fa4ae55d6a565476
Charley <ctpugmire@gmail.com>
2023-02-22 20:22:02 -0600

    set time range to "short_term"

--- _data/spotify.js
+++ _data/spotify.js
@@ -3,11 +3,14 @@ require('dotenv').config();
 const EleventyFetch = require('@11ty/eleventy-fetch');
 const fetch = require('node-fetch');
 
+// https://developer.spotify.com/documentation/web-api/reference/#/operations/get-users-top-artists-and-tracks
 const ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/me/top/artists';
 const TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/top/tracks';
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 // Only get the top 5
 const LIMIT = 5;
+// Set to short_term to get some variation
+const TIME_RANGE = 'short_term';
 
 module.exports = async function () {
   // Base64 encode the auth
@@ -32,7 +35,12 @@ module.exports = async function () {
     .then((json) => json.access_token)
     .catch(console.err);
 
-  const artists = await EleventyFetch(`${ARTISTS_ENDPOINT}?limit=${LIMIT}`, {
+  // Build artists query
+  const artistsUrl = new URL(ARTISTS_ENDPOINT);
+  artistsUrl.searchParams.set('limit', LIMIT);
+  artistsUrl.searchParams.set('time_range', TIME_RANGE);
+
+  const artists = await EleventyFetch(artistsUrl.toString(), {
     duration: '1h',
     type: 'json',
     fetchOptions: {
@@ -42,7 +50,12 @@ module.exports = async function () {
     },
   });
 
-  const tracks = await EleventyFetch(`${TRACKS_ENDPOINT}?limit=${LIMIT}`, {
+  // Build tracks query
+  const tracksUrl = new URL(TRACKS_ENDPOINT);
+  tracksUrl.searchParams.set('limit', LIMIT);
+  tracksUrl.searchParams.set('time_range', TIME_RANGE);
+
+  const tracks = await EleventyFetch(tracksUrl.toString(), {
     duration: '1h',
     type: 'json',
     fetchOptions: {
